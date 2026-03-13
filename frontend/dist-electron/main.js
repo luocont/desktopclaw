a6ac38dc2abcfb73271a480f45aad0c9d5591462
username1637 <2302947694@qq.com>
2026-03-13 09:19:32 +0800

    提交前端

---
+++ frontend/dist-electron/main.js
@@ -0,0 +1,37 @@
+"use strict";
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const isDev = process.env.NODE_ENV === "development";
+let win;
+const createWindow = () => {
+  if (win) return;
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    x: 0,
+    y: 0,
+    webPreferences: {
+      preload: path.resolve(__dirname, "preload.js"),
+      devTools: isDev,
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+    //alwaysOnTop: true
+  });
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+};
+app.on("ready", () => {
+  createWindow();
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
